6cff2718323b76d818c0f560b9431cd8b51cb0f8
beahamad <ana.hamad@ccc.ufcg.edu.br>
2022-07-13 17:53:35 -0300

    13/07/2022: Comparing in shell script

--- lrnShell/ex8.sh
+++ lrnShell/ex8.sh
@@ -10,3 +10,26 @@
 # -u: check if it has suid set (set user id)
 # -r: readability
 # -w: writable
+
+# if a file exists
+if [ -e ex1.sh ] 
+then 
+	echo "ex1.sh exists"
+fi
+
+# if a file is executable
+if [ -x ex8.sh ]
+then
+	echo "ex8 is executable"
+else
+	echo "ex8 isn't executable"
+fi
+
+# if a file is a directory
+if [ -d ex1.sh ]
+then
+	echo "ex1 is a directory"
+else
+	echo "ex1 isn't a directory"
+fi
+
